b8ebf2b23aacb3e4142852801e5e8c4826e4b739
Charlton Sammut <sammut026@gmail.com>
2021-05-23 18:36:02 +0200

    Made changes to experiment 1

--- src/experiments/experiment1.py
+++ src/experiments/experiment1.py
@@ -17,8 +17,6 @@ if __name__ == "__main__":
         possible inputs = "stocknet" or "KDD17"
     """""
 
-    os.chdir("../Adv-ALSTM")
-
     # Get arguments
     args = []
 
@@ -31,24 +29,28 @@ if __name__ == "__main__":
         if type(args[i]) is str:
             args[i] = args[i].lower()
 
+    os.chdir("../Adv-ALSTM")
+
     if args[1] == "stocknet" or args[1] == '0' or args[1] == 0:
         if args[0] == "lstm" or args[0] == '0' or args[0] == 0:
-            os.system("python pred_lstm.py -a 0 -l 10 -u 32 -l2 10 -f 1 -p ../../data/stocknet/ourpped -qs ../../models/lstm-stocknet/lstm-stocknet")
+            os.system("python pred_lstm.py -a 0 -l 10 -u 32 -l2 10 -f 1 -p ../../data/stocknet/ourpped -qs ../../models/lstm-Stocknet/lstm-Stocknet")
         elif args[0] == "alstm" or args[0] == '1' or args[1] == 0:
-            os.system("python pred_lstm.py -l 5 -u 4 -l2 1 -f 1 -p ../../data/stocknet/ourpped -qs ../../models/alstm-stocknet/alstm-stocknet")
+            os.system("python pred_lstm.py -l 5 -u 4 -l2 1 -f 1 -p ../../data/stocknet/ourpped -qs ../../models/alstm-Stocknet/alstm-Stocknet")
         elif args[0] == "adv-alstm" or args[0] == "advalstm" or args[0] == '2' or args[0] == 2:
-            if not os.path.exists("../../models/alstm-stocknet/alstm-stocknet"):
-                os.system("python pred_lstm.py -l 5 -u 4 -l2 1 -f 1 -p ../../data/stocknet/ourpped -qs ../../models/alstm-stocknet/alstm-stocknet")
-            os.system("python pred_lstm.py -l 5 -u 4 -l2 1 -v 1 -rl 1 -q ../../models/alstm-stocknet/alstm-stocknet -la 0.01 -le 0.05 --p ../../data/stocknet/ourpped -qs ../../models/advAlstm-stocknet/advAlstm-stocknet")
+            if not os.path.exists("../../models/alstm-Stocknet/alstm-Stocknet.meta"):
+                os.system("python pred_lstm.py -l 5 -u 4 -l2 1 -f 1 -p ../../data/stocknet/ourpped -qs ../../models/alstm-Stocknet/alstm-Stocknet")
+            os.system("python pred_lstm.py -l 5 -u 4 -l2 1 -v 1 -rl 1 -q ../../models/alstm-Stocknet/alstm-Stocknet -la 0.01 -le 0.05 --p ../../data/stocknet/ourpped -qs ../../models/advAlstm-Stocknet/advAlstm-Stocknet")
+        else:
+            print("Argument error: " + str(args[0]) + " not a valid ML model")
     elif args[1] == "kdd17" or args[1] == '1' or args[1] == 1:
         if args[0] == "lstm" or args[0] == '0' or args[1] == 0:
             os.system("python pred_lstm.py -p ../../data/kdd17/ourpped/ -l 5 -u 4 -l2 0.001 -a 0 -f 1 -qs ../../models/lstm-KDD17/lstm-KDD17")
         elif args[0] == "alstm" or args[0] == '1' or args[1] == 1:
             os.system("python pred_lstm.py -p ../../data/kdd17/ourpped/ -l 15 -u 16 -l2 0.001 -f 1 -qs ../../models/alstm-KDD17/alstm-KDD17")
         elif args[0] == "adv-alstm" or args[0] == "advalstm" or args[0] == '2' or args[1] == 2:
-            if not os.path.exists("../../models/alstm-stocknet/alstm-stocknet"):
+            if not os.path.exists("../../models/alstm-KDD17/alstm-KDD17.meta"):
                 os.system("python pred_lstm.py -p ../../data/kdd17/ourpped/ -l 15 -u 16 -l2 0.001 -f 1 -qs ../../models/alstm-KDD17/alstm-KDD17")
-            os.system("python pred_lstm.py -p ../../data/kdd17/ourpped/ -l 15 -u 16 -l2 0.001 -v 1 -rl 1 -q ../../models/advAlstm-stocknet/advAlstm-stocknet -la 0.05 -le 0.001 -f 1 -qs ../../models/advAlstm-KDD17/advAlstm-KDD17")
+            os.system("python pred_lstm.py -p ../../data/kdd17/ourpped/ -l 15 -u 16 -l2 0.001 -v 1 -rl 1 -q ../../models/alstm-KDD17/alstm-KDD17 -la 0.05 -le 0.001 -f 1 -qs ../../models/advAlstm-KDD17/advAlstm-KDD17")
         else:
             print("Argument error: " + str(args[0]) + " not a valid ML model")
     else:
